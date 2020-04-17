aa0c70979d80ab4b9b3dc90c40715663a90000a3
yukasaito718 <bumbumbanana718@gmail.com>
2020-04-17 12:57:05 +0900

    finish lesson8 slot

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <!-- scope でこのファイルを修飾することを設定する-->
 <!--  // パスカルケースLikeHeader ケバブケースlike-header htmlとの見分けがつきやすいのでパスカルで-->
 <!--// app内でしか使えないローカル登録-->
+<!-- v-slot:引数で slotタグに値を渡せる-->
+<!--名前付きvslotがないときはcomponentにdefaultを指定する※<LikeHeader v-slot:default="slotPlops">※省略は#default-->
+
 
 
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{ number }}</h2>
+    <LikeHeader>
+      <template v-slot:title="slotProps">
+        <h2>こんにちは</h2>
+        <h2>{{slotProps.user.firstName}}</h2>
+      </template>
+      <template v-slot:default>
+        <p>皆さん </p>
+        <p>初めまして</p>
+      </template>
+      <template v-slot:number>
+        <p>{{ number }}</p>
+      </template>
+      <template #[title]></template>
+    </LikeHeader>
+
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
     <LikeNumber :total-number="number"></LikeNumber>
   </div>
@@ -18,8 +34,9 @@ import LikeHeader from "./components/LikeHeader.vue";
 export default {
   data() {
     return {
-      number: 14
-    };
+      number: 14,
+      title: "title" 
+    }
   },
   components: {
     LikeHeader
--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>トータルのいいね数</h1>
+  <div>
+    <slot name="title" :user="user" text="text"></slot>
+    <slot></slot>
+    <hr>
+    <p>いいねの数</p>
+    <slot name="number"></slot>
+  </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: {
+          firstName: "Jack",
+          lastName: "Donald"
+        }
+      }
+    }
+}
+</script>
