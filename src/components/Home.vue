801e80ffb2bf96681621ae48492cf21418ec8763
yukasaito718 <bumbumbanana718@gmail.com>
2020-04-18 17:15:37 +0900

    start lesson10 カスタムディレクティブ/フック関数

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,3 @@
 <template>
-    <p>Home</p>
+    <p v-border>Home</p>
 </template>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,28 @@ import LikeNumber from "./components/LikeNumber.vue";
 Vue.config.productionTip = false;
 // グローバル登録
 Vue.component("LikeNumber", LikeNumber);
+Vue.direcrive("border",{
+  //フック関数
+  bind(el,binding,vnode) {
+    //ディレクディブで初めて対象の要素に紐付いた時
+  },
+  inserted(el,binding,vnode) {
+    // 親Node（実DOM）に挿入された時
+
+  },
+  update(el,binding,vnode,oldVnode) {
+    //コンポーネントが更新される度。子コンポーネントが更新される前
+
+  },
+  componentUpdated(el,binding,vnode,oldVnode) {
+    //コンポーネントが更新される度。子コンポーネントが更新された後
+
+  },
+  unbind(el,binding,vnode) {
+    //ディレクティブが紐付いている要素から取り除かれた時
+
+  },
+});
 
 new Vue({
   render: h => h(App)
