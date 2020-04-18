0e578e1d467674875fc0fe58e737be6aa7ecad56
yukasaito718 <bumbumbanana718@gmail.com>
2020-04-18 18:19:37 +0900

    finish lesson10

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,26 @@
 <template>
-    <p v-border:solid.round.shadow="{width: '10px', color: 'blue'}">Home</p>
+    <p v-border:solid.round.shadow="{width: '10px', color: 'blue'}">{{tempData}}</p>
 </template>
 
 <script>
+//カスタムディレクティブをローカル登録することもできる
 export default {
+    data() {
+        return {
+            tmpDta: "hello"
+        };
+    },
+    //ローカル登録内ではthis.が使えないので注意する
     directives: {
         border(el, binding){
-            //データを受け取りたいときはbinding.valueで受け取れる
-            //複数データを受け取りたいときは送り側をオブジェクトにして、受け取り側でプロパティを指定して取得する
             el.style.borderWidth = binding.value.width;
             el.style.borderColor = binding.value.color;
-            //ディレクティブの引数を取得したいときはargument;をつける
             el.style.borderStyle = binding.arg;
-            // if文も使える※roundはbooleanで返ってくるので以下の条件式で良い
             if (binding.modifiers.round){
-            el.style.borderRadius = "0.5rem";
+                el.style.borderRadius = "0.5rem";
             }
             if (binding.modifiers.shadow){
-            el.style.boxShadow = "0 2px 5px rgba(0,0,0,1)";
+                el.style.boxShadow = "0 2px 5px rgba(0,0,0,1)";
             }
         }
     }
