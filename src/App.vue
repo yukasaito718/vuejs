9a6f28b25cf10890c85bed5e2049f2e89536cc25
yukasaito718 <bumbumbanana718@gmail.com>
2020-04-20 14:51:00 +0900

    トランジショングループ

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
+// :css=""cssを使用しない宣言
 <template>
   <div class="main">
     <button @click="myAnimation = 'slide'">Slide</button>
     <button @click="myAnimation = 'fade'">Fade</button>
     <p>{{myAnimation}}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <!-- transition-groupは複数の要素に対して使う事ができる 必ずkey属性をつける -->
+      <transition-group name="fade">
+      <li 
+        style="cursor: poi"
+        v-for="(number, index) in numbers" 
+        :key="number" 
+        @click="remove(index)" 
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
+    <br><br>
+    <transition
+      :css="false"
+      @befor-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
+    <br>
+    <button @click="myComponent = 'ComponentA'">Slide</button>
+    <button @click="myComponent = 'ComponentB'">Fade</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
     <!-- mode属性 要素のタイミングをずらす 複数の要素がある場合はつけるとスムーズになる -->
     <transition name="fade" mode="out-in">
       <!-- transitionはコンテンツ内のタグが変わった時に発火するため同じタグ同士だと動かない keyを指定することによって違うタグとして判断する -->
@@ -16,6 +46,7 @@
         key="hello"
         >こんにちは</p>
     </transition>
+
     <transition 
       enter-active-class="animated bounce" 
       leave-active-class="animated shake" 
@@ -35,17 +66,93 @@
 </template>
 
 <script>
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
 export default {
+  components: {
+    ComponentA,
+    ComponentB
+  },
   data() {
     return {
+      numbers: [ 0, 1, 2 ],
       show: true,
-      myAnimation: "slide"
+      myAnimation: "slide",
+      myComponent: "ComponentA"
     };
+  },
+  // doneはjsの動作が終わったことを教える関数※非同期によく使う elはhtmlのこと
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      // 現れる前
+      el.style.transform = "scale(0)";
+    },
+    enter(el,done) {
+      // 現れる時
+      let scale = 0;
+      const interval = setInterval(() =>{
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1;
+        if( scale > 1 ){
+          clearInterval(interval);
+          done();
+        }
+      }, 30);
+    },
+    // afterEnter(el) {
+    //   // 現れた後
+    // },
+    // enterCancelled(el) {
+    //   // 現れるアニメーションがキャンセルされた時
+    // },
+    // beforeLeave(el) {
+    //   // 消える前
+    // },
+    leave(el,done) {
+      // 消える時
+      let scale = 1;
+      const interval = setInterval(() =>{
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1;
+        if( scale < 0 ){
+          clearInterval(interval);
+          done();
+        }
+      }, 30);
+    }
+    // afterLeave(el) {
+    //   // 消えた後
+    // },
+    // leaveCancelled(el) {
+    //   // 消えるアニメーションがキャンセルされた時※v-showの時のみ実行される
+    // }
+
   }
 };
 </script>
 
 <style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: deeppink;
+}
+.fade-move {
+  transition: transform 10s;
+}
+
 .fade-enter {
 /* 現れる時の最初の状態 */
   opacity: 0;
@@ -65,6 +172,8 @@ export default {
 .fade-leave-active {
 /* 消える時のトランジションの状態 （どのように消えるのか？）*/
   transition: opacity 0.5s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
 /* 消える時の最後の状態 */
